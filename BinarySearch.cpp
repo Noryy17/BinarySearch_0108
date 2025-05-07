cc3e7c1adc7ea63af35625ab131573c5a4649608
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-05-07 12:01:16 +0700

    Membuat Display Untuk Ditampilkan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -49,5 +49,21 @@ void bubbleSortArray()
 }
 
 void display()
+{
+    cout << "\n=========================================\n";
+    cout << " Elemen Array Setelah Dijurutkan (Asc) \n";
+    cout << "=========================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+}
+
+
 
   
